8e62bc208222e14d0ed9e5f5c8907998708444df
Mine Brown <minecrc411@gmail.com>
2026-02-17 21:36:47 +0700

    Trigger security pipeline

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,10 +28,10 @@ import { GatewayModule } from './gateway/gateway.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: 5432,
       username:  process.env.DB_USER,
       password:  process.env.DB_PASSWORD,
-      database: 'postgres',
+      database: process.env.DB_NAME,
+      port: parseInt(process.env.DB_PORT || '5432'),
       autoLoadEntities: true,
       synchronize: true,
     }),
